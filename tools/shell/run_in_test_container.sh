7820564cf91fa84d9d8f54973e87e66d3b08447f
Justin Morgan <2justinmorgan@gmail.com>
2023-07-24 22:58:21 -0700

    Move func run_in_test_container to script

---
+++ tools/shell/run_in_test_container.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+. tools/shell/common/functions.sh
+
+function build_test_container() {
+	local image_tag_name="$1"
+	local workdir_path="$2"
+
+	DOCKER_BUILDKIT=1 docker build \
+		. \
+		--tag "$image_tag_name" \
+		--target "testing" \
+		--build-arg CW_TESTING_WORKDIR_PATH="$workdir_path" ||
+		exit_err
+}
+
+function run_test_container() {
+	local image_tag_name="$1"
+	local workdir_path="$2"
+	local container_id
+	local container_name
+	local container_status
+
+	container_name="$image_tag_name"
+	container_id="$(docker ps -aqf "name=^${container_name}$")" || exit_err
+
+	if [ -n "$container_id" ]; then
+		container_status="$(docker container inspect -f '{{.State.Status}}' "$container_id")" || exit_err
+		[ "$container_status" == "running" ] && echo "$container_id" && return
+		docker start "$container_name" || exit_err
+		return
+	fi
+
+	docker run \
+		--workdir "$workdir_path" \
+		--interactive \
+		--tty \
+		--detach \
+		--name "$container_name" \
+		"$image_tag_name" ||
+		exit_err
+}
+
+function run_in_test_container() {
+	local exec_str="$1"
+	local image_tag_name
+	local workdir_path
+	local container_id
+	local path
+
+	image_tag_name="cw-testing"
+	workdir_path="/cw_testing"
+
+	if [ -z "$(docker images -q "$image_tag_name")" ]; then
+		build_test_container "$image_tag_name" "$workdir_path" || exit_err
+	fi
+
+	container_id="$(run_test_container "$image_tag_name" "$workdir_path")" || exit_err
+
+	for path in $(git ls-files | cut -d'/' -f1 | sort | uniq); do
+		docker cp "$path" "${container_id}:${workdir_path}"
+	done
+
+	echo "execing '$exec_str' in container '$container_id'"
+
+	# shellcheck disable=SC2086 # reason: the exec str may have args
+	docker exec --tty "$container_id" ${exec_str} || exit_err
+}
+
+function check_sources() {
+	check_sourced_functions || exit 1
+}
+
+function main() {
+	local exec_str="$*"
+
+	check_sources
+
+	run_in_test_container "$exec_str"
+}
+
+main "$@"
--- tools/shell/testing/lint.sh
+++ tools/shell/testing/lint.sh
@@ -2,25 +2,6 @@
 
 . tools/shell/common/functions.sh
 
-function check_args() {
-	local argv_0="$1"
-	local argv_1="$2"
-	local flag_help
-	local flag_docker
-	local usage_msg
-
-	flag_help="--help"
-	flag_docker="--docker"
-	usage_msg="Usage: $(basename "$argv_0") [$flag_help|$flag_docker]"
-
-	[ -z "$argv_1" ] && return
-
-	[ "$argv_1" == "$flag_help" ] && echo "$usage_msg" && exit
-	[ "$argv_1" == "$flag_docker" ] && return
-	echo >&2 -e "${usage_msg}\ninvalid arg '$argv_1'"
-	exit_err
-}
-
 function lint_sh() {
 	local path
 
@@ -35,20 +16,10 @@ function check_sources() {
 	check_sourced_functions || exit 1
 }
 
-function run() {
-	local argv_0="$1"
-	local argv_1="$2"
-
-	check_args "$argv_0" "$argv_1"
-
+function main() {
 	check_sources
-
-	if [ "$argv_1" == "--docker" ]; then
-		run_in_test_container "$argv_0" || exit_err
-		exit 0
-	fi
 
 	lint_sh
 }
 
-run "$0" "$1"
+main
