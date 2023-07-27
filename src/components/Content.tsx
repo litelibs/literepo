4df046e3e313843049fba00e51570da6d20bd4f1
Justin Morgan <2justinmorgan@gmail.com>
2023-07-27 00:30:10 -0700

    Init component FileLink

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,6 +1,14 @@
 "use client";
 import { Text, Spacer } from "@nextui-org/react";
 import { Box } from "../components/Box";
+import { FileLink } from "../components/FileLink";
+
+const createFileLinks = (fileNames: string[]): JSX.Element => {
+  const fileLinks: JSX.Element[] = [];
+  for (let i = 0; i < fileNames.length; i++)
+    fileLinks.push(<FileLink fileName={fileNames[i]} />);
+  return <>{fileLinks}</>;
+};
 
 type Props = {
   fileNames: string[];
@@ -8,7 +16,7 @@ type Props = {
 
 export const Content = ({ fileNames }: Props) => (
   <Box css={{ px: "$12", mt: "$8", "@xsMax": { px: "$10" } }}>
-    {fileNames}
+    {createFileLinks(fileNames)}
     <Text h2>Lorem ipsum dolor sit amet</Text>
     <Text size="$lg">
       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
