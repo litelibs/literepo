23a0f41a3a0f78e05c8dddc0a6465d4a96303312
Justin Morgan <2justinmorgan@gmail.com>
2023-07-29 00:37:55 -0700

    Install apply animate links via framer-motion

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { NextUIProvider } from "@nextui-org/react";
-import { JSX, useEffect, useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import { JSX } from "react";
 import { Box } from "../components/Box";
 import { FileLink } from "../components/FileLink";
 import { FileNav } from "../components/FileNav";
@@ -28,22 +29,26 @@ export const Content = ({
   startPath,
   fileContent,
 }: Props): JSX.Element => {
-  const [isSsr, setIsSsr] = useState(true);
   const rootDir = File.constructFromPaths(filePaths);
   const currFile = File.getFromRoot(rootDir, startPath);
 
-  useEffect(() => setIsSsr(false), []);
-
-  if (isSsr) return <></>;
-
   return (
-    <NextUIProvider>
-      <Box css={{ px: "$14", mt: "$8", "@xsMax": { px: "$10" } }}>
-        <FileNav files={currFile.toFilePath()} currFile={currFile} />
-        {createFileLinks(Object.values(currFile.children))}
-        <br />
-        {fileContent}
-      </Box>
+    <NextUIProvider key={window?.location?.pathname}>
+      <AnimatePresence>
+        <motion.div
+          initial={{ opacity: 0, y: 10 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: 10 }}
+          transition={{ delay: 0.01 }}
+        >
+          <Box css={{ px: "$14", mt: "$8", "@xsMax": { px: "$10" } }}>
+            <FileNav files={currFile.toFilePath()} currFile={currFile} />
+            {createFileLinks(Object.values(currFile.children))}
+            <br />
+            {fileContent}
+          </Box>
+        </motion.div>
+      </AnimatePresence>
     </NextUIProvider>
   );
 };
